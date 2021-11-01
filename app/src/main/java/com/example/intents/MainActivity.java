07895efdfc5d40aa83f1997348ee80c6be47120a
anas012 <anas@salesassist.pk>
2021-11-01 22:47:32 +0500

    using intents to move on next activity and retrieve data from previous acitivity

--- app/src/main/java/com/example/intents/MainActivity.java
+++ app/src/main/java/com/example/intents/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button button =(Button)findViewById(R.id.button1);
+        EditText edit=findViewById(R.id.edittext1);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(Intent.ACTION_DIAL);
-                intent.setData(Uri.parse("tel:" + "03334470545"));
+//                Intent intent = new Intent(Intent.ACTION_DIAL);
+//                intent.setData(Uri.parse("tel:" + "03334470545"));
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                intent.putExtra("value",edit.getText().toString());
                 startActivity(intent);
             }
     });
--- app/src/main/java/com/example/intents/MainActivity2.java
+++ app/src/main/java/com/example/intents/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.intents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        TextView text=findViewById(R.id.textView2);
+        Intent intent=getIntent();
+        String val = intent.getStringExtra("value");
+        text.setText(val);
     }
 }
